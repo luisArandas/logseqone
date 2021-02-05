65895cece2c27640a150f8bb2dfebdcd8796a776
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-02-05 14:41:40 +0000

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;; Please set the path accordingly, and don’t check in your changes
+;; or they will overwrite the default for everyone else.
+((nil . ((org-roam-directory . "~/logseqone/pages/")
+         (org-roam-db-location . "~/logseqone/org-roam.db"))))
